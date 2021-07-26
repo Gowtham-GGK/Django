6b757708c22a25c3b8c426ed6a67b0a63c1e9fd8
Gowtham-GGK <gowthamaaju@gmail.com>
2021-07-26 18:41:25 +0530

    Created blog_home app

--- django_blog/blog/apps.py
+++ django_blog/blog/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class BlogHomeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'blog_home'
+    name = 'blog'
--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog_home.urls'))
+    path('', include('blog.urls'))
 ]
